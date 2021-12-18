c983351126efe8b67f40eb4d39405cfeb57e0184
Gneya <80591287+Gneya@users.noreply.github.com>
2021-12-18 23:57:39 +0530

    DAY-1

---
+++ Array/count_the_zeros.java
@@ -0,0 +1,15 @@
+class count_the_zeros {
+    int countZeroes(int[] arr, int n) {
+        // code here
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
