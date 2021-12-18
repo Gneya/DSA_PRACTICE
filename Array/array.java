88836c4b41322db636882f3adc48989305601fbc
Gneya <80591287+Gneya@users.noreply.github.com>
2021-12-19 00:01:14 +0530

    DAY-1

---
+++ Array/array.java
@@ -0,0 +1,22 @@
+class array
+{
+    public static ArrayList<Integer> array(int a[][], int b[], int n)
+    {
+        int sum=0;
+        int max=0;
+        // Complete the function
+        for(int i=0;i<n;i++)
+        {
+            sum+=a[i][i];
+            if(max<b[i])
+            {
+                max=b[i];
+            }
+            
+        }
+        ArrayList<Integer> result=new ArrayList();
+        result.add(sum);
+        result.add(max);
+        return result;
+    }
+}
