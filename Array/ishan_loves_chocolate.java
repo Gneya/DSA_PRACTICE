e2f7f68397abd0ad8ae5afca8f3710282da31676
Gneya <80591287+Gneya@users.noreply.github.com>
2021-12-19 23:18:20 +0530

    DAY-2

---
+++ Array/ishan_loves_chocolate.java
@@ -0,0 +1,12 @@
+class ishan_loves_chocolate{
+    
+   
+    // Function for finding maximum and value pair
+    public static int chocolates (int arr[], int n) {
+        //Complete the function
+        Arrays.sort(arr);
+        return arr[0];
+    }
+    
+    
+}
