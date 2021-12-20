341005a5a0cccb05e4dcc24e369bb4b4fb0137fd
Gneya <80591287+Gneya@users.noreply.github.com>
2021-12-20 23:03:36 +0530

    DAY-3

---
+++ Array/balanced_array.java
@@ -0,0 +1,19 @@
+class balanced_array
+{
+    long minValueToBalance(long a[] ,int n)
+    {
+        int low=0;
+        int high=n-1;
+        int leftsum=0;
+        int rightsum=0;
+         while(low<high)
+        {
+           leftsum+=a[low];
+           rightsum+=a[high];
+           low++;
+           high--;
+        }
+        return Math.abs(leftsum-rightsum);
+
+    }
+}
