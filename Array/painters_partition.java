35b9af634e2cb3aab6e5a2ea8c4e01133f7660ff
Gneya Pandya <80591287+Gneya@users.noreply.github.com>
2021-12-24 22:56:22 +0530

    DAY-7

---
+++ Array/painters_partition.java
@@ -0,0 +1,34 @@
+class painters_partition {
+    // Function to find maximum product subarray
+    long maxProduct(int[] arr, int n) {
+        long curr=1;
+        long product=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            curr*=arr[i];
+            if(product<curr)
+            {
+                product=curr;
+            }
+            if(curr==0)
+            {
+                curr=1;
+            }
+        }
+        curr=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            curr*=arr[i];
+            if(product<curr)
+            {
+                product=curr;
+            }
+            if(curr==0)
+            {
+                curr=1;
+            }
+        }
+        return product;
+        // code here
+    }
+}
