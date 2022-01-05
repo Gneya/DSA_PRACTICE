acfc8b306e0d28c3a8adc04fc60e95607bff5d5c
Gneya Pandya <80591287+Gneya@users.noreply.github.com>
2022-01-05 23:26:13 +0530

    DAY-17

---
+++ if_sorted.cpp
@@ -0,0 +1,18 @@
+class Solution{   
+public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        if(n==0||n==1)
+        {
+            return true;
+        }
+        if(arr[0]>arr[1])
+        {
+            return false;
+        }
+        else
+        {
+            return arraySortedOrNot(arr+1,n-1);
+        }
+    }
+};
